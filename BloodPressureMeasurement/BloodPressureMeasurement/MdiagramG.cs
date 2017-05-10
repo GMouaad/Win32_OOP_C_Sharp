182ecf4cd715344e2feac7f215a754726a167cb1
Mouaad Gssair <Mgsair@gmail.com>
2017-05-10 15:49:22 +0200

    Panel Changes

--- BloodPressureMeasurement/BloodPressureMeasurement/MdiagramG.cs
+++ BloodPressureMeasurement/BloodPressureMeasurement/MdiagramG.cs
@@ -25,7 +25,20 @@ namespace BloodPressureMeasurement
             g.DrawLine(Pens.Gray, 80, 110, 620, 110);
             g.DrawLine(Pens.Gray, 80, 130, 620, 130);
             g.DrawLine(Pens.Gray, 80, 150, 620, 150);
-            g.DrawLine(Pens.Black, 80, 10, 640, 30);
+            g.DrawLine(Pens.Gray, 80, 170, 620, 170);
+            g.DrawLine(Pens.Gray, 80, 190, 620, 190);
+            g.DrawLine(Pens.Gray, 80, 210, 620, 210);
+            g.DrawLine(Pens.Gray, 80, 230, 620, 230);
+            g.DrawLine(Pens.Gray, 80, 250, 620, 250);
+            g.DrawLine(Pens.Gray, 80, 270, 620, 270);
+            g.DrawLine(Pens.Gray, 80, 290, 620, 290);
+            g.DrawLine(Pens.Gray, 80, 310, 620, 310);
+            g.DrawLine(Pens.Gray, 80, 330, 620, 330);
+            g.DrawLine(Pens.Gray, 80, 350, 620, 350);
+            g.DrawLine(Pens.Gray, 80, 370, 620, 370);
+            g.DrawLine(Pens.Black, 80, 30, 80, 390);
+            g.DrawLine(Pens.Black, 80, 390, 640, 390);
+
 
         }
     }
